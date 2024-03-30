4352c933e612f4d8c06348fae778802dac58b25b
nnoor <147066929+Nowaisir@users.noreply.github.com>
2024-03-30 15:18:02 +0000

    Format python files

--- src/main.py
+++ src/main.py
@@ -14,8 +14,10 @@ from PySide6.QtWidgets import (
 from PySide6 import QtCore
 from PySide6.QtGui import QFont
 
+
 class ExecutableSegmentNotFound(Exception):
     pass
+
 
 class Instruction:
     operands = []
@@ -26,20 +28,22 @@ class Instruction:
     def asRichText(self):
         colors = {
             "UNKNOWN": "#A48A85",
-            "OTHER": "#464646", # the opcode, punctuation, etc.
-            "REGISTER": "#295F21"
+            "OTHER": "#464646",  # the opcode, punctuation, etc.
+            "REGISTER": "#295F21",
         }
 
         for hexColorCode in colors.values():
-            assert(hexColorCode[0] == "#") # might make this mistake
+            assert hexColorCode[0] == "#"  # might make this mistake
 
         if self.opcode == None:
             return f"<font color={colors['UNKNOWN']}>Unknown Instruction</font>"
         else:
             return self.opcode
 
+
 print(Instruction("ecall").asRichText())
 print(Instruction("efence").asRichText())
+
 
 def findInstructionsRange(fileBytes):
     # Use segment header table to locate executable segment
@@ -50,7 +54,9 @@ def findInstructionsRange(fileBytes):
     instructionsSegment = None
 
     for header in range(
-        SEGMENT_HEADER_TABLE, SEGMENT_HEADER_TABLE + numSegments * SEGMENT_HEADER_SIZE, SEGMENT_HEADER_SIZE
+        SEGMENT_HEADER_TABLE,
+        SEGMENT_HEADER_TABLE + numSegments * SEGMENT_HEADER_SIZE,
+        SEGMENT_HEADER_SIZE,
     ):
         segmentFlags = int.from_bytes(fileBytes[header + 4 : header + 8], "little")
 
@@ -59,29 +65,32 @@ def findInstructionsRange(fileBytes):
             # our heuristic is that the first segment whose memory is executable
             # is the one which all instructions are under
             instructionsSegment = dict(
-                offset = int.from_bytes(fileBytes[header + 8:header + 0x10], "little"),
-                virtAddr = int.from_bytes(fileBytes[header + 0x10:header + 0x18], "little"),
-                size = int.from_bytes(fileBytes[header + 0x20: header + 0x28], "little")
+                offset=int.from_bytes(fileBytes[header + 8 : header + 0x10], "little"),
+                virtAddr=int.from_bytes(
+                    fileBytes[header + 0x10 : header + 0x18], "little"
+                ),
+                size=int.from_bytes(fileBytes[header + 0x20 : header + 0x28], "little"),
             )
-
 
     if instructionsSegment == None:
         raise ExecutableSegmentNotFound
 
-
     # we need the entry point offset in the file but the ELF file only
     # explicitly stores its virtual address. We do however have all the
     # properties of the segment it probably lies in
-    entryPointVirtualAddress = int.from_bytes(fileBytes[0x18: 0x20], "little")
+    entryPointVirtualAddress = int.from_bytes(fileBytes[0x18:0x20], "little")
     toEntryPoint = entryPointVirtualAddress - instructionsSegment["virtAddr"]
 
     entryPoint = instructionsSegment["offset"] + toEntryPoint
 
     # each instruction is 4 bytes long, hence the 4
-    return range(entryPoint, instructionsSegment["offset"] + instructionsSegment["size"], 4)
+    return range(
+        entryPoint, instructionsSegment["offset"] + instructionsSegment["size"], 4
+    )
+
 
 def formattedAddressNumbers(fileBytes):
-    entryPointVirtAddr = int.from_bytes(fileBytes[0x18: 0x20], "little")
+    entryPointVirtAddr = int.from_bytes(fileBytes[0x18:0x20], "little")
     numInstructions = len(findInstructionsRange(fileBytes))
 
     addresses = (entryPointVirtAddr + 4 * i for i in range(numInstructions))
@@ -89,12 +98,13 @@ def formattedAddressNumbers(fileBytes):
     # e.g a range() of [1024, 1028, 1032, 1036] into
     # 0x400, 0x404, 0x408 0x40C
     raw = (hex(addr).title() for addr in addresses)
-    pruned = (addr[2:] for addr in raw) # 0xF04c -> F04C
+    pruned = (addr[2:] for addr in raw)  # 0xF04c -> F04C
 
     # e.g 0000 0400, 000F 30FC, etc.
     beautified = (addr.zfill(8)[0:4] + " " + addr.zfill(8)[4:8] for addr in pruned)
 
-    return "\n".join(beautified) # as this is used in a QLabel
+    return "\n".join(beautified)  # as this is used in a QLabel
+
 
 def decodeInstructions(fileBytes):
     instructions = []
@@ -109,8 +119,9 @@ def decodeInstructions(fileBytes):
     markup = "<br>".join(instr.asRichText() for instr in instructions)
 
     print(repr(markup))
-    
+
     return markup
+
 
 def chooseFile():
     filePath = QFileDialog.getOpenFileName()[0]
@@ -147,7 +158,7 @@ Your architecture ({hex(architecture)}) isn't supported",
 
         return
 
-    try: # subsequent calls to findInstructionsRange() are safe
+    try:  # subsequent calls to findInstructionsRange() are safe
         findInstructionsRange(fileBytes)
     except ExecutableSegmentNotFound:
         QMessageBox.warning(
@@ -159,12 +170,16 @@ Your architecture ({hex(architecture)}) isn't supported",
         return
 
     # Passed all checks after this point
-    primaryLayout.setCurrentIndex(1) # switch to the disassembly view
+    primaryLayout.setCurrentIndex(1)  # switch to the disassembly view
     addressNumbers.setText(formattedAddressNumbers(fileBytes))
-    addressNumbers.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+    addressNumbers.setSizePolicy(
+        QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred
+    )
 
     assemblyColumn.setText(decodeInstructions(fileBytes))
-    assemblyColumn.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+    assemblyColumn.setSizePolicy(
+        QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred
+    )
 
 
 app = QApplication()
@@ -172,7 +187,9 @@ window = QWidget()  # widgets with no parents specified create their own window
 window.setWindowTitle("Disassembler")
 window.show()
 
-primaryLayout = QStackedLayout() # allows us to switch between the start screen and disassembly view
+primaryLayout = (
+    QStackedLayout()
+)  # allows us to switch between the start screen and disassembly view
 window.setLayout(primaryLayout)
 
 startScreen = QWidget()
@@ -180,11 +197,17 @@ startScreen.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expan
 primaryLayout.addWidget(startScreen)
 
 disassemblyScreen = QWidget()
-disassemblyScreen.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-primaryLayout.addWidget(disassemblyScreen) # order of adding must correspond to the .setCurrentIndexCall()
+disassemblyScreen.setSizePolicy(
+    QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+)
+primaryLayout.addWidget(
+    disassemblyScreen
+)  # order of adding must correspond to the .setCurrentIndexCall()
 
 disassemblyScreenLayout = QHBoxLayout()
-disassemblyScreenLayout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop | QtCore.Qt.AlignmentFlag.AlignLeft)
+disassemblyScreenLayout.setAlignment(
+    QtCore.Qt.AlignmentFlag.AlignTop | QtCore.Qt.AlignmentFlag.AlignLeft
+)
 disassemblyScreenLayout.setContentsMargins(15, 10, 0, 0)
 disassemblyScreen.setLayout(disassemblyScreenLayout)
 
@@ -196,7 +219,9 @@ disassemblyScreenLayout.addWidget(addressNumbers)
 
 # same size and properties of the addressNumbers above
 assemblyColumn = QLabel()
-assemblyColumn.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft | QtCore.Qt.AlignmentFlag.AlignVCenter)
+assemblyColumn.setAlignment(
+    QtCore.Qt.AlignmentFlag.AlignLeft | QtCore.Qt.AlignmentFlag.AlignVCenter
+)
 assemblyColumn.setFont(QFont("Courier New", 12))
 disassemblyScreenLayout.addWidget(assemblyColumn)
 
